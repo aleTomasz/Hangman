edb2203449bb30df64cc9a543a207925a55b6209
o3emil <emil.ozon@gmail.com>
2024-02-06 19:37:14 +0100

    poprawiony quit

--- hangman.py
+++ hangman.py
@@ -1,7 +1,7 @@
 import random
 import graphics
 
-graphic=graphics.graphic
+graphic = graphics.graphic
 dificult_level_dict = {
     1: 'easy',
     2: 'medium',
@@ -23,28 +23,20 @@ def set_dificult_level():
 
 
 def word_choose():
-
-
     with open('countries-and-capitals.txt', 'r') as file:
-      words=file.read()
-    countries=[]
-    capitals=[]
-    words2=words.split('\n')
+        words = file.read()
+    countries = []
+    capitals = []
+    words2 = words.split('\n')
     words2.pop()
     for i in words2:
-        pair_of_words=i.split(' | ')
+        pair_of_words = i.split(' | ')
 
         countries.append(pair_of_words[0])
         capitals.append(pair_of_words[1])
 
-    words=countries+capitals
+    words = countries + capitals
     return random.choice(words)
-
-
-
-
-
-
 
 
 def hide_word(word_to_guess):
@@ -68,11 +60,11 @@ def hangmen(dificult_level, word_to_guess):
         if "_" in hiden_word:
             print(''.join(hiden_word))
             letter = input('Give letter: ').lower()
-            if letter==''or len(letter)>1:
-                continue
-            if (letter == 'quit'):
+            if letter == 'quit':
                 print('you quit the game')
                 break
+            if letter == '' or len(letter) > 1:
+                continue
             elif letter in list_of_word_lower and not letter in already_tried_letters:
                 for index in range(len(hiden_word)):
                     if list_of_word_lower[index] == letter:
@@ -90,6 +82,7 @@ def hangmen(dificult_level, word_to_guess):
             print(''.join(hiden_word))
             print('YOU WIN!!!')
             break
+
 
 while True:
     word_to_guess = word_choose()
